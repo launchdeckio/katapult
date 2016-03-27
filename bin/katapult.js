f9382548e6314dcbc4f4e7ffef1e95640207900d
JM Versteeg <janmerijnversteeg@gmail.com>
2016-03-27 17:08:22 +0200

    Add better depcheck implementation

--- bin/katapult.js
+++ bin/katapult.js
@@ -3,6 +3,11 @@
 
 const _ = require('lodash');
 
+const updateNotifier = require('update-notifier');
+const pkg            = require('./package.json');
+
+updateNotifier({pkg}).notify();
+
 let yargs = require('yargs').usage('$0 <cmd> [args]');
 
 _.each(require('./../lib/cli/actions'), Action => (new Action()).register(yargs));
