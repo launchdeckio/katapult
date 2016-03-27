77668f852eff4265014d1e1412f9ae376c159287
JM Versteeg <janmerijnversteeg@gmail.com>
2016-03-27 15:35:09 +0200

    Add parse ttl

---
+++ test/common/util/parseTtl.js
@@ -0,0 +1,28 @@
+'use strict';
+
+require('./../../support/support');
+
+const parseTtl = require('./../../../lib/common/util/parseTtl');
+const expect   = require('chai').expect;
+
+describe('parseTtl', () => {
+
+    it('should parse various formats of ttl', () => {
+
+        parseTtl(100).should.equal(100);
+        parseTtl('100').should.equal(100);
+        parseTtl('100seconds').should.equal(100000);
+        parseTtl('100second').should.equal(100000);
+        parseTtl('100 seconds').should.equal(100000);
+        parseTtl('100 second').should.equal(100000);
+        parseTtl('100s').should.equal(100000);
+        parseTtl('100 s').should.equal(100000);
+
+        parseTtl('1h').should.equal(60 * 60 * 1000);
+
+        parseTtl('1d').should.equal(24 * 60 * 60 * 1000);
+
+        expect(parseTtl('foobar')).to.be.null;
+        expect(parseTtl()).to.be.null;
+    });
+});
