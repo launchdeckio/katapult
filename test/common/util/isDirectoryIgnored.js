0e8637a5a7a202723a697e755ee177489f2641ff
JM Versteeg <janmerijnversteeg@gmail.com>
2016-03-29 18:09:51 +0200

    Add some tests for isDirectoryIgnored

---
+++ test/common/util/isDirectoryIgnored.js
@@ -0,0 +1,35 @@
+'use strict';
+
+require('./../../support/support');
+
+const isDirectoryIgnored = require('./../../../lib/common/util/isDirectoryIgnored');
+
+const fga = require('./../../../lib/common/util/globUtils').flattenGlobArray;
+
+const idi = (directory, ignores, globalIgnores) => isDirectoryIgnored(directory, ignores ? fga(ignores) : [], globalIgnores);
+
+describe('isDirectoryIgnored', () => {
+
+    it('should determine whether to ignore the given directory based on the given globs', () => {
+
+        idi('foobar').should.be.false;
+        idi('foobar', ['foobar']).should.be.true;
+        idi('foobar', ['!foobar']).should.be.false;
+
+        idi('foobar/baz', ['foobar']).should.be.false;
+        // This one should be false, because the directory 'foobar' will never have been reached thus foobar/baz will
+        // not be tried
+
+        idi('foobar', ['!foobar/baz']).should.be.false;
+        idi('foobar/baz', ['!foobar/baz']).should.be.false;
+        idi('foobar/baz', ['foobar/baz']).should.be.true;
+        idi('foobar/baz', ['foobar', '!foobar/baz']).should.be.false;
+    });
+
+    it('should also take into account the globalIgnores, if given', () => {
+
+        idi('.idea').should.be.false;
+        idi('.idea', [], ['.idea']).should.be.true;
+        idi('folder/.idea', [], ['.idea']).should.be.true;
+    });
+});
