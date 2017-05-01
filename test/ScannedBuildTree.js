5b85b9284a8031edd16793703dc46a55a6662091
JM Versteeg <janmerijnversteeg@gmail.com>
2017-05-01 17:12:44 +0200

    rename BuildTree to ScannedBuildTree

--- test/ScannedBuildTree.js
+++ test/ScannedBuildTree.js
@@ -2,18 +2,18 @@
 
 require('./support/support');
 
-const rewire      = require('rewire');
-const path        = require('path');
-const BuildTree   = rewire('./../lib/BuildTree');
-const MockFs      = require('q-io/fs-mock');
-const classicMock = require('mock-fs');
-const constants   = require('./../constants.json');
+const rewire           = require('rewire');
+const path             = require('path');
+const ScannedBuildTree = rewire('./../lib/ScannedBuildTree');
+const MockFs           = require('q-io/fs-mock');
+const classicMock      = require('mock-fs');
+const constants        = require('./../constants.json');
 
 const configFile = constants.configFile;
 const ignoreFile = constants.ignoreFile;
 
 const setupMockFs = function (mockFs) {
-    BuildTree.__set__('fs', MockFs(mockFs));
+    ScannedBuildTree.__set__('fs', MockFs(mockFs));
     classicMock(mockFs);
 };
 
@@ -27,18 +27,18 @@ const expected = [
     path.join('tmp', 'a', 'b', 'c', configFile)
 ];
 
-describe('BuildTree', () => {
+describe('ScannedBuildTree', () => {
 
     describe('getConfigFiles()', () => {
 
         it('should get an array of the config files present in the given directory', function () {
             setupMockFs(require('./fixtures/filesystems/testIgnores'));
-            return BuildTree.getConfigFiles('tmp').should.eventually.deep.equal(expected);
+            return ScannedBuildTree.getConfigFiles('tmp').should.eventually.deep.equal(expected);
         });
 
         it('should work with a .katapultignore blacklist', function () {
             setupMockFs(require('./fixtures/filesystems/testIgnoreBlacklist'));
-            return BuildTree.getConfigFiles('tmp').should.eventually.deep.equal(expected);
+            return ScannedBuildTree.getConfigFiles('tmp').should.eventually.deep.equal(expected);
         });
 
         afterEach(function () {
