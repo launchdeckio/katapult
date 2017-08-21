217a6c5b2fe8acd785ff646d13f5bff359ee9e12
JM Versteeg <janmerijnversteeg@gmail.com>
2017-08-21 19:59:34 +0200

    remove unused imports from test support file

--- test/support/support.js
+++ test/support/support.js
@@ -4,10 +4,6 @@ const chai           = require("chai");
 const sinonChai      = require("sinon-chai");
 const chaiAsPromised = require("chai-as-promised");
 
-const Promise        = require('bluebird');
-const readAsPromised = Promise.promisify(require('fs').readFile);
-const sinon          = require('sinon');
-
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
