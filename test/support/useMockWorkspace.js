a63a63791339fd80f5a6cc759ec4878321a808bb
JM Versteeg <janmerijnversteeg@gmail.com>
2017-08-21 14:01:43 +0200

    remove unnecessary console calls

--- test/support/useMockWorkspace.js
+++ test/support/useMockWorkspace.js
@@ -10,16 +10,12 @@ module.exports = () => {
 
     return {
         before: function () {
-
             this.timeout(10000);
             mockWorkspace = new MockWorkspace(path.resolve(__dirname + '/../test-tmp'));
-            console.log('run setup');
             return mockWorkspace.setup();
         },
         after:  function () {
-
             this.timeout(10000);
-            console.log('run teardown');
             return mockWorkspace.tearDown();
         },
         get:    () => mockWorkspace,
